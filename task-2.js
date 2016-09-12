5d8a542bfe8500f0427877a069b4a06a69d3b5ca
orlindraganov <orlindraganov@gmail.com>
2016-09-12 10:16:20 +0300

    Refactored some spaghetti and added some comments

--- task-2.js
+++ task-2.js
@@ -49,28 +49,27 @@ function solve() {
         }
 
         add(product) {
-            //CHAINING
             this._products.push(product);
+
+            //In order to provide chaining:
             return this;
         }
 
         remove(product) {
-            var productIndex = this._products.indexOf(product);
-            if (productIndex === -1) {
-                throw new ProductNotFoundException("Product not found");
-            }
-
             if (this._products.length === 0) {
                 throw new ShoppingCartEmptyException("Shopping cart is empty");
+            }
+
+            var productIndex = this._products.indexOf(product);
+
+            if (productIndex === -1) {
+                throw new ProductNotFoundException("Product not found");
             }
 
             this._products.splice(productIndex, 1);
         }
 
         showCost() {
-            if (this.products.length === 0) {
-                return 0;
-            }
             var cost = 0;
 
             this.products.forEach(function (product) {
@@ -82,15 +81,15 @@ function solve() {
 
         showProductTypes() {
             var productTypes = [],
-                prType,
+                productType,
                 productTypeIndex;
 
             this.products.forEach(function (product) {
-                prType = product.productType;
-                productTypeIndex = productTypes.indexOf(prType);
+                productType = product.productType;
+                productTypeIndex = productTypes.indexOf(productType);
 
                 if (productTypeIndex === -1) {
-                    productTypes.push(prType);
+                    productTypes.push(productType);
                 }
             });
 
@@ -100,6 +99,7 @@ function solve() {
         }
 
         getInfo() {
+            //Directly pasted from StackOverflow
             function dynamicSort(property) {
                 var sortOrder = 1;
                 if (property[0] === "-") {
@@ -111,38 +111,39 @@ function solve() {
                     return result * sortOrder;
                 }
             }
-            if (this.products.length === 0) {
-                return { products: [], totalPrice: 0 };
-            }
+            //End of paste
 
-            var prods = this.products.slice();
-            var info = {
-                products: [],
-                totalPrice: 0
-            };
+            var prods = this.products.slice(),
+                len = this.products.length,
+                info = {
+                    products: [],
+                    totalPrice: 0
+                };
 
+            //How to use dynamicSort:
             prods.sort(dynamicSort("name"));
 
-            info.products.push(
-                {
-                    name: prods[0].name,
-                    totalPrice: prods[0].price,
-                    quantity: 1
-                });
-
-            info.totalPrice = prods[0].price;
-
-            for (var i = 1; i < prods.length; i += 1) {
-                if (prods[i - 1].name != prods[i].name) {
+            for (var i = 0; i < prods.length; i += 1) {
+                if (i === 0) {
                     info.products.push(
                         {
                             name: prods[i].name,
                             totalPrice: prods[i].price,
                             quantity: 1
                         });
+
+                    info.totalPrice += prods[i].price;
+                } else if (prods[i - 1].name != prods[i].name) {
+                    info.products.push(
+                        {
+                            name: prods[i].name,
+                            totalPrice: prods[i].price,
+                            quantity: 1
+                        });
+
                     info.totalPrice += prods[i].price;
                 } else {
-                    let len = info.products.length;
+                    len = info.products.length;
                     info.totalPrice += prods[i].price;
                     info.products[len - 1].totalPrice += prods[i].price;
                     info.products[len - 1].quantity += 1;
